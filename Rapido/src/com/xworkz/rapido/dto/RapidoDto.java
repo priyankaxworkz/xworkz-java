a7a45680b5713ce939a458a5e4ec021309be1b44
priyanka <priyankaxworkz@gmail.com>
2024-01-23 04:11:13 -0800

    dto with list

---
+++ Rapido/src/com/xworkz/rapido/dto/RapidoDto.java
@@ -0,0 +1,12 @@
+package com.xworkz.rapido.dto;
+
+import lombok.Data;
+
+@Data
+public class RapidoDto {
+
+	private String location;
+	private String manager;
+	private int totalVehicles;
+	private int totalBranch;
+}
